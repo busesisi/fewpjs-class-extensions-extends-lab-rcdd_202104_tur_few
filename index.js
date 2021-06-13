6a7aff71b83ce874694ebba352de21b473ff8cf4
Buse Şentürk <buse.senturk@outlook.com>
2021-06-13 16:44:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+        this.sides = sides
+        this.count = this.sides.length
+    }
+
+    get countSides() {
+        return this.sides.length
+      }
+    
+      get perimeter() {
+        if (!Array.isArray(this.sides)) return;
+        let sum = 0;
+        for (var int of this.sides) {
+          sum += int
+        }
+        return sum
+      }
+    
+  }// Your code here
+
+
+  
